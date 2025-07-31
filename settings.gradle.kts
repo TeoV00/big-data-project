a4d9effdd1de5394202bb655ba3700f2a8695a31
Luca Tassinari <luca.tassinari.2000@gmail.com>
2025-07-31 12:44:24 +0200

    chore project config

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,15 @@
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.10.0"
+    id("com.gradle.develocity") version "3.19.2"
 }
 
 rootProject.name = "google-local-reviews-analysis"
+
+develocity {
+    buildScan {
+        termsOfUseUrl = "https://gradle.com/terms-of-service"
+        termsOfUseAgree = "yes"
+        uploadInBackground = !System.getenv("CI").toBoolean()
+        publishing.onlyIf { it.buildResult.failures.isNotEmpty() }
+    }
+}
